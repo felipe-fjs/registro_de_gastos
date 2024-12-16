1f06ea211d75b0249f5792caa88c240a5f1f4c65
Felipe Joaquim <felipejoaquimtech@gmail.com>
2024-12-16 10:40:12 -0300

    Update __init__.py
    alterando local do db

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/meudb.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost:3306/gerenciar_gastos'
 
 db = SQLAlchemy(app)
 
