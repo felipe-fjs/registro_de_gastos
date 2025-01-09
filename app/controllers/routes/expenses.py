e0d10cbbb09868104e2bda0a20ca12f3716ccd8b
Felipe Joaquim <felipejoaquimtech@gmail.com>
2025-01-08 21:57:08 -0300

    adicionando parte do link para novo gasto
    adicio

--- app/controllers/routes/expenses.py
+++ app/controllers/routes/expenses.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, request, render_template
+from app import db
+from flask import Blueprint, request, render_template, redirect, url_for
+from sqlalchemy.exc import SQLAlchemyError
+from ...models.expenses import Expenses
 
 expenses_route = Blueprint('expenses', __name__)
 
@@ -6,13 +9,19 @@ expenses_route = Blueprint('expenses', __name__)
 # @login_required
 def new_expense():
     if request.method == 'POST':
-        print(f'''
-                titulo: {request.form.get('title')}
-                total: {request.form.get('total')}
-                categorie: {request.form.get('category')}
-                local: {request.form.get('local')}
-                data: {request.form.get('data')}
-            ''')
+        try:
+            new_exp = Expenses()
+            new_exp.title = request.form.get('title')   
+            new_exp.category = request.form.get('category')   
+            new_exp.total = request.form.get('total')   
+            new_exp.local = request.form.get('local')   
+            new_exp.data = request.form.get('data')   
+            with db.session as conn:
+                conn.add(new_exp)
+            return redirect(url_for('expenses.new_url'))
+        except SQLAlchemyError:
+            pass
+        pass
 
     return render_template("expenses/new.html")
 
