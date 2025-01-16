a80b05fb8a445581c6f0e1e1dd2f3834cdd3ea13
Felipe Joaquim <felipejoaquimtech@gmail.com>
2025-01-16 11:10:15 -0300

    adicionando rota para categories ao init e seu template

--- app/controllers/routes/__init__.py
+++ app/controllers/routes/__init__.py
@@ -1,4 +1,6 @@
 from .expenses import expenses_route
+from .categories import categories_route
 from app import app
 
 app.register_blueprint(expenses_route)
+app.register_blueprint(categories_route)
--- app/controllers/routes/expenses.py
+++ app/controllers/routes/expenses.py
@@ -4,7 +4,6 @@ from sqlalchemy.exc import SQLAlchemyError
 from ...models.expenses import Expenses
 from ...models.local import Local   
 from ...models.category import Category
-import datetime
 
 expenses_route = Blueprint('expenses', __name__)
 
@@ -15,21 +14,20 @@ def new_expense():
         try:
             # cria o novo gasto
             new_exp = Expenses()
-            new_exp.title = request.form.get('title')   
-            new_exp.category_id = "1"
+            new_exp.title = request.form.get('title')
+            new_exp.category_id = request.form.get('category_id')
             new_exp.amount = request.form.get('total')   
-            new_exp.local_id = "1"
+            new_exp.local_id = request.form.get('local_id')
             new_exp.date = request.form.get('data')
-            print(new_exp.date)
+
             # registra o novo gasto
             db.session.add(new_exp)
             db.session.commit()
-            print("FOI")
         except SQLAlchemyError:
-            print("NÃO FOI")
-            flash(f"Ocorreu um erro ao registrar o gasto '{request.form.get('title')}'")
+            flash(f"Ocorreu um erro ao registrar o gasto '{request.form.get('title')}'!")
     
         return redirect(url_for('expenses.new_expense'))
+    
     # pegar informações de categorias e loais já cadastrados em bando de dados
     try:
         local = Local.query.all()
