87fac1d9dbfb36e35e73341e112c974b45a0dd10
Felipe Joaquim <felipejoaquimtech@gmail.com>
2025-01-18 10:25:09 -0300

    add routes e templates para conta (login, signup)

--- app/controllers/routes/__init__.py
+++ app/controllers/routes/__init__.py
@@ -1,6 +1,8 @@
 from .expenses import expenses_route
 from .categories import categories_route
+from .account import account_route
 from app import app
 
 app.register_blueprint(expenses_route)
 app.register_blueprint(categories_route)
+app.register_blueprint(account_route)
--- app/controllers/routes/account.py
+++ app/controllers/routes/account.py
@@ -1,7 +1,12 @@
 from app import db, login_manager, bcrypt
+from app.models.user import User
 from flask import Blueprint,  render_template, request, redirect, url_for
 
 account_route = Blueprint('account', __name__)
+
+@login_manager.user_loader
+def get_user(id):
+    return User.query.filter_by(user_id=id).first()
 
 @account_route.route('/login', methods=['GET', 'POST'])
 def login():
