35fc8352c0f09db4d83da50a5a70d7c51f590f42
Felipe Joaquim <felipejoaquimtech@gmail.com>
2025-01-19 16:12:11 -0300

    Update account.py
    rota para verificar existencia de email no ato de cadastro

--- app/controllers/routes/account.py
+++ app/controllers/routes/account.py
@@ -1,6 +1,6 @@
 from app import db, login_manager, bcrypt
 from app.models.user import User
-from flask import Blueprint,  render_template, request, redirect, url_for
+from flask import Blueprint,  render_template, request, redirect, url_for, jsonify
 
 account_route = Blueprint('account', __name__)
 
@@ -18,6 +18,15 @@ def login():
 @account_route.route('/cadastro', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
+        # email já é verificado no formulário com um link a parte
         pass
 
     return render_template('account/signup.html')
+
+@account_route.route('/verify-email/', defaults={'email':None})
+@account_route.route('/verify-email/<email>')
+def verify_email(email):
+    if User.query.filter_by(email=email).first():
+        return jsonify({'exists':True})
+    
+    return jsonify({'exists':False})
