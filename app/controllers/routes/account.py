e897bdb3c09688c37bf6552ed96826ccb3527f0a
Felipe Joaquim <felipejoaquimtech@gmail.com>
2025-01-20 11:20:43 -0300

    Update account.py
    add logica para cadastro de usuário e seu perfil

--- app/controllers/routes/account.py
+++ app/controllers/routes/account.py
@@ -1,6 +1,7 @@
-from app import db, login_manager, bcrypt
-from app.models.user import User
-from flask import Blueprint,  render_template, request, redirect, url_for, jsonify
+from app import db, login_manager
+from app.models.user import User, Profile
+from flask import Blueprint,  render_template, request, redirect, url_for, jsonify, flash
+from sqlalchemy.exc import SQLAlchemyError
 
 account_route = Blueprint('account', __name__)
 
@@ -19,8 +20,30 @@ def login():
 def signup():
     if request.method == 'POST':
         # email já é verificado no formulário com um link a parte
-        pass
+        try:
+            new_user = User()
+            new_user.email = request.form.get('email')
+            new_user.new_pwd(request.form.get('pwd'))
 
+            db.session.add(new_user)
+            db.session.flush()
+
+            new_profile = Profile()
+            new_profile.user_id = new_user.id
+            new_profile.name = request.form.get('name')
+            new_profile.birth = request.form.get('birth')
+
+            db.session.add(new_profile)
+
+            db.session.commit()
+
+        except SQLAlchemyError:
+            flash('Ocorreu um erro ao tentar se registrar!')
+            return redirect(url_for('account.signup'))
+        
+        flash('Cadastro realizado com sucesso!')
+        return redirect(url_for('account.login'))
+    
     return render_template('account/signup.html')
 
 @account_route.route('/verify-email/', defaults={'email':None})
