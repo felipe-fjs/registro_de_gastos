e9a1c45c08d4a74607dba95525df8d333a987ff2
Felipe Joaquim <felipejoaquimtech@gmail.com>
2025-02-20 14:17:39 -0300

    Update __init__.py
    registro do blueprint locals_route

--- app/controllers/routes/__init__.py
+++ app/controllers/routes/__init__.py
@@ -1,8 +1,10 @@
 from .expenses import expenses_route
 from .categories import categories_route
 from .account import account_route
+from .locals import locals_route
 from app import app
 
 app.register_blueprint(expenses_route)
 app.register_blueprint(categories_route)
 app.register_blueprint(account_route)
+app.register_blueprint(locals_route)
